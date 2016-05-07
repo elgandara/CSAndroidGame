3d27e8bdfab7a678d4a28f56feb96dfb6c445171
uriel-munoz <uriel.munozantonio@gmail.com>
2016-05-06 19:58:35 -0700

    added Dictionary and DAFSA

---
+++ app/src/main/java/com/android/cs/csandroidgame/Dictionary.java
@@ -0,0 +1,71 @@
+package com.android.cs.csandroidgame;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/**
+ * Created by uriel on 5/6/2016.
+ *
+ *
+ * connect the taken with the DAFSA
+ *
+ *
+ */
+
+
+
+public class Dictionary {
+    public final static int MIN_LENGTH=1;
+    private ArrayList<String> taken;
+    private ArrayList<String> words;
+
+    public Dictionary(InputStream wordStream) throws IOException{
+        BufferedReader in = new BufferedReader(new InputStreamReader(wordStream));
+        words = new ArrayList<>();
+        taken = new ArrayList<>();
+
+        String line = null;
+        while((line = in.readLine())!=null)
+        {
+            String word = line.trim();
+            if(word.length()>=MIN_LENGTH)
+                words.add(line.trim());
+        }
+    }
+
+
+    public boolean isWord(String word){return words.contains(word);}
+
+
+    public String getPossibleWord(String start)
+    {
+
+        return null;
+    }
+
+    public boolean isWordTaken(String word)
+    {
+        return taken.contains(word);
+    }
+
+    public boolean removeWord(String word)
+    {
+        taken.add(word);
+        return words.remove(word);
+
+    }
+    public boolean addWord(String word)
+    {
+        return words.add(word);
+    }
+    public boolean reset()
+    {
+        taken.clear();
+        return true;
+        //taken = new DAFSA();
+    }
+
+}
